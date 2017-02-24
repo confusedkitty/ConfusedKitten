58e6097470b8e5161d094203994563bd99e65c61
confusedkitty <wangyue921128@163.com>
2017-02-24 16:41:15 +0800

    新建实体helper类， 用于属性相同的两个实体互相赋值（属性不区分大小写）

---
+++ Common/ModelHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ConfusedKitten.Common
+{
+    public static class ModelHelper
+    {
+        /// <summary>
+        ///  将实体A的属性赋值给实体B
+        /// </summary>
+        /// <typeparam name="T1">类型（数据源）</typeparam>
+        /// <typeparam name="T2">类型（赋值实体）</typeparam>
+        /// <param name="tFrom">数据源</param>
+        /// <param name="tTo">赋值实体</param>
+        /// <returns></returns>
+        public static T2 ExchangeClass<T1, T2>(T1 tFrom, T2 tTo)
+        {
+            // 取得属性集合
+            PropertyInfo[] proFrom = tFrom.GetType().GetProperties();
+            PropertyInfo[] proTo = tTo.GetType().GetProperties();
+
+            try
+            {
+                // 遍历数据源属性
+                foreach (PropertyInfo info in proFrom)
+                {
+                    // 获取属性值
+                    string proValue = info.GetValue(tFrom, null)?.ToString();
+                    if (string.IsNullOrEmpty(proValue))
+                    {
+                        continue;
+                    }
+                    // 取得赋值实体相同名称属性(不区分大小写)
+                    PropertyInfo toTemp = proTo.FirstOrDefault(p => p.Name.ToLower().Equals(info.Name.ToLower()));
+                    if (toTemp != null)
+                    {
+                        // 设置新实体属性值
+                        toTemp.SetValue(tTo, proValue, null);
+                    }
+                }
+                return tTo;
+            }
+            catch
+            {
+                // 返回新实例
+                return tTo;
+            }
+        }
+    }
+}
---
+++ MyWebRequest/RequestMethod.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Text;
+
+namespace ConfusedKitten.MyWebRequest
+{
+    public static class RequestMethod
+    {
+        /// <summary>
+        ///  post提交方法
+        /// </summary>
+        /// <param name="url">路径</param>
+        /// <param name="data">数据</param>
+        /// <returns></returns>
+        public static string PostMethod(string url, string data)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    //编码，尤其是汉字，事先要看下抓取网页的编码方式  
+                    byte[] postData = Encoding.GetEncoding("UTF-8").GetBytes(data);
+                    //采取POST方式必须加的header，如果改为GET方式的话就去掉这句话即可  
+                    webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                    //得到返回字符流  
+                    byte[] responseData = webClient.UploadData(url, "POST", postData);
+                    //解码 
+                    string srcString = Encoding.GetEncoding("UTF-8").GetString(responseData);
+                    return srcString;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        ///  get提交方法
+        /// </summary>
+        /// <param name="url">路径</param>
+        /// <returns></returns>
+        public static string GetMethod(string url)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    string srcString = webClient.DownloadString(url);
+                    return srcString;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
